e4832006414671b7cbc4c858d4d97716cf423350
aparna10064 <72874041+aparna10064@users.noreply.github.com>
2021-06-19 11:49:03 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,2 +1,57 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
+int max(int a[], int n)
+{
+	int max = a[0];
+	for(int i=0; i<n; i++)
+	{
+		if(a[i]>max)
+		max= a[i]; 
+	}
+	return max;
+}
+
+int min(int a[], int n)  
+{
+	int min;
+	min = a[0];
+	for(int i=0; i<n; i++) 
+	{
+		if(a[i]<min)
+		min =a[i];
+	}
+	return min;
+}
+
+float average(int avg[], int n)
+{
+
+	int sum = 0;
+	float avg =0;
+	for(int i=0; i<n; i++) 
+	{
+		sum = sum+a[i];
+	}
+	avg = (float)sum/n;
+	return avg;
+}
+int mode(int arr[], int n)
+{
+	int counts[n];
+	int c;
+	for(int i=0; i<n; i++)
+	{
+		n = arr[i];
+		for(int j=0; j<n; j++)
+		{
+			if(a[j] == n)
+			c += 1;
+		}
+	counts[i] = c;
+	}
+	
+	
+
+
+
+	
