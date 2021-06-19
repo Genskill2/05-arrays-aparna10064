4c9bdc8b2f65444935fc9c21b35bd411787fcf53
aparna10064 <72874041+aparna10064@users.noreply.github.com>
2021-06-19 12:25:28 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,8 +1,10 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
+#include <assert.h>
+/*Maximum*/
 int max(int a[], int n)
 {
-	int max = a[0];
+	int max = 0;
 	for(int i=0; i<n; i++)
 	{
 		if(a[i]>max)
@@ -11,10 +13,11 @@ int max(int a[], int n)
 	return max;
 }
 
+/*Minimum*/
 int min(int a[], int n)  
 {
 	int min;
-	min = a[0];
+	min = 0;
 	for(int i=0; i<n; i++) 
 	{
 		if(a[i]<min)
